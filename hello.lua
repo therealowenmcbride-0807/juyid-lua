ba195fd790fe13523d8f6a9f51a5ee4a04d9b966
therealowenmcbride-0807 <therealowenmcbride@gmail.com>
2026-04-17 13:31:20 -0400

    Add files via upload

--- hello.lua
+++ hello.lua
@@ -1,3 +1,3 @@
 local juyid = require("juyid")
 
-io.write(juyid.style.set_forground_color(50,50,50), "Hello", juyid.style.set_forground_color(100,20,90), "world!", juyid.style.reset())
+io.write("Hello world!\n", juyid.style.bold("Hello world but bold!\n"), juyid.style.set_color_scheme(32, 44), "Hello world but green text on a blue backdrop!\n", juyid.style.reset())
--- juyid/style.lua
+++ juyid/style.lua
@@ -1,36 +1,38 @@
-local escape_codes = {}
+local style = {}
 
-function escape_codes.reset()
+function style.reset()
 
     local result = "\x1b[0m"
 
     return result
 end
 
-function escape_codes.bold(string)
+function style.bold(string)
 
-    local result = "\x1b[1m" + string + "\x1b[22m"
-
-    return result
-end
-
-function escape_codes.set_color_scheme(fg, bg)
-
-    local result = "\x1b[" .. fg
-end
-
-function escape_codes.set_fg_color(r, g, b)
-
-    local result = "\x1b[38;5;" .. r .. ";" .. g .. ";" .. b .. "m"
+    local result = "\x1b[1m" .. string .. "\x1b[22m"
 
     return result
 end
 
-function escape_codes.set_bg_color(r, g, b)
+function style.set_color_scheme(fg, bg)
 
-    local result = "\x1b[48;5;" .. r .. ";" .. g .. ";" .. b .. "m"
+    local result = "\x1b[" .. fg .. ";" .. bg .. "m"
 
     return result
 end
 
-return escape_codes
+function style.set_fg_color(r, g, b)
+
+    local result = "\x1b[38;2;" .. r .. ";" .. g .. ";" .. b .. "m"
+
+    return result
+end
+
+function style.set_bg_color(r, g, b)
+
+    local result = "\x1b[48;2;" .. r .. ";" .. g .. ";" .. b .. "m"
+
+    return result
+end
+
+return style
